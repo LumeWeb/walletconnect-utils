e03219518b4401914d3bf8f55d90f2ad25321f99
Derrick Hammer <derrick@derrickhammer.com>
2022-05-31 06:58:58 -0400

    *Implement crypto.subtle api fallback

--- misc/environment/src/crypto.ts
+++ misc/environment/src/crypto.ts
@@ -1,14 +1,22 @@
+import { Crypto } from "@peculiar/webcrypto";
+
 export function getBrowerCrypto(): Crypto {
-  // @ts-ignore
-  return global?.crypto || global?.msCrypto || {};
+    // @ts-ignore
+    return global?.crypto || global?.msCrypto || {};
 }
 
 export function getSubtleCrypto(): SubtleCrypto {
-  const browserCrypto = getBrowerCrypto();
-  // @ts-ignore
-  return browserCrypto.subtle || browserCrypto.webkitSubtle;
+    const browserCrypto = getBrowerCrypto();
+    // @ts-ignore
+    let subtle = browserCrypto.subtle || browserCrypto.webkitSubtle;
+
+    if (browserCrypto && !subtle) {
+        subtle = (new Crypto()).subtle;
+    }
+
+    return subtle;
 }
 
 export function isBrowserCryptoAvailable(): boolean {
-  return !!getBrowerCrypto() && !!getSubtleCrypto();
+    return !!getBrowerCrypto() && !!getSubtleCrypto();
 }
